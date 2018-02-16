be088879f475cdad2af93d1a0b63906f0c73ae2f
Dipti Ranjan Sahu <diptisahu11@gmail.com>
2018-02-16 15:31:03 +0530

    Changed myTypes.vhd
    type to subtype and natural to integer

--- quartus/myTypes.vhd
+++ quartus/myTypes.vhd
@@ -5,8 +5,8 @@ use ieee_proposed.fixed_pkg.all;
 use work.all;
 
 package myTypes is
-constant fp_int : natural :=4;
-constant fp_frac : natural := -3;
-constant fp_bits : natural :=fp_int-fp_frac+1;
-type fp sfixed (fp_int downto fp_frac);
+constant fp_int : integer :=4;
+constant fp_frac : integer := -3;
+constant fp_bits : integer := fp_int-fp_frac+1;
+subtype fp is sfixed (fp_int downto fp_frac);
 end package myTypes;
