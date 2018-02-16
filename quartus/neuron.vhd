5a073df45536e269a9aa27e7ceef774d47d947c9
Dipti Ranjan Sahu <diptisahu11@gmail.com>
2018-02-16 15:34:23 +0530

    Neuron entity is complete

--- quartus/neuron.vhd
+++ quartus/neuron.vhd
@@ -1,30 +1,50 @@
-
--- Neuron --
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
 library ieee_proposed;
+
+--use model:
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee_proposed.math_utility_pkg.all;
 use ieee_proposed.fixed_pkg.all;
-use work.all;
+use work.myTypes.all;
+
 entity neuron is
-port(Iapp,Isyn : in fp;
-		clk : in std_logic;
-		spike: out std_logic);
-end entity neuron;
+    port (
+	Iapp,Isyn : in fp;
+	clk : in std_logic;
+	spike: out std_logic);
+end entity;
 
-architecture behav of neuron is
-component register_n is 
-generic(bits:natural:=4);
-port(clk: in std_logic;
-rst: in std_logic;
-dataIn : in std_logic_vector(bits-1 downto 0);
-dataOut : out std_logic_vector(bits-1 downto 0);
-end;
+architecture Behave of neuron is
 
-signal Vn,aVn,Iapp_b1,Isyn_b1,sum_I,Vn1: fp;
-signal b1,b2,alpha: fp:=to_sfixed(1.0,4,-3);
-constant Vth : sfixed = to_sfixed(1.0,4,-3);
-begin 
-	regV : register_n generic map(bits=>fp_bits) port map (clk=>clk,rst=>spike,dataIn=>Vn1,dataOut=>Vn);
-	compV : VthComparator port map(V=>Vn1,Vth=>Vth,spike=>spike);
-	Vn1<=Iapp*b1+Isyn*b2+alpha*Vn;
-end behav;
+   component register_fp
+    generic(bits :natural :=4);
+    port(
+         clk : in std_logic;
+			rst : in std_logic;
+			dataIn : in fp;
+			dataOut : out fp
+        );
+    end component;
+	 
+	 component VthComparator is
+		generic(bits: natural:=8);
+		port( V,Vth : in fp;
+			spike: out std_logic);
+	 end component;
+
+	 signal spik : std_logic;
+    signal Vn,aVn,Iapp_b1,Isyn_b1,sum_I,Vn1 : fp;
+    signal b1,b2,alpha : fp := to_sfixed(1.0,4,-3);
+    signal Vth : fp := to_sfixed(1.0,4,-3);
+	
+    begin 
+		--b1 <= to_sfixed(1.0,fp_int,fp_frac);
+		--alpha <= to_sfixed(1.0,fp_int,fp_frac);
+		--b2 <= to_sfixed(1.0,fp_int,fp_frac);
+		regV : register_fp generic map(bits=>fp_bits) port map (clk=>clk,rst=>spik,dataIn=>Vn1,dataOut=>Vn);
+		compV : VthComparator port map(V=>Vn1,Vth=>Vth,spike=>spik);
+		Vn1 <= resize(Iapp*b1 + Isyn*b2 + alpha*Vn,fp_int,fp_frac);
+		spike <= spik;
+
+end Behave;
