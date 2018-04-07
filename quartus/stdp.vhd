8ecbaf6f9026faf3d332c2699290d234343c1c16
Amritesh Sharma <Amritesh07071996@gmail.com>
2018-04-07 22:00:42 +0530

    modified stdp

--- quartus/stdp.vhd
+++ quartus/stdp.vhd
@@ -13,7 +13,7 @@ use work.all;
 
 entity wrapper_stdp is
 port(spikeSynapse, spikeNeuron,clk : in std_logic;
-		w: out std_logic_vector(7 downto 0));
+		w: out std_logic_vector(fp_bits-1 downto 0));
 end entity wrapper_stdp;
 
 architecture behave of wrapper_stdp is 
@@ -44,12 +44,12 @@ use ieee_proposed.fixed_pkg.all;
 use work.myTypes.all;
 use work.all;
 
-entity STDP is
+entity STDP_old is
 port(spikeSynapse, spikeNeuron,clk : in std_logic;
 		w: out fp);
-end entity STDP;
+end entity STDP_old;
 
-architecture behav of STDP is
+architecture behav of STDP_old is
 
 	component register_fp is 
 		port(clk: in std_logic;
@@ -70,15 +70,15 @@ architecture behav of STDP is
 	signal spikeNeuronSLV : std_logic_vector(0 downto 0);
 	signal alpha1 : fp :=to_sfixed(1,fp_int,fp_frac);
 	signal alpha2 : fp :=to_sfixed(-1,fp_int,fp_frac);
-	signal alpha3 : fp :=to_sfixed(0.9,fp_int,fp_frac);
-	signal alpha4 : fp :=to_sfixed(0.9,fp_int,fp_frac);
+	signal alpha3 : fp :=to_sfixed(0.4,fp_int,fp_frac);
+	signal alpha4 : fp :=to_sfixed(0.4,fp_int,fp_frac);
 
 begin 
 
 	alpha1<= to_sfixed(1,alpha1);
 	alpha2<= to_sfixed(-1,alpha2);
-	alpha3<= to_sfixed(0.7, alpha3);
-	alpha4<= to_sfixed(0.7, alpha4);
+	alpha3<= to_sfixed(0.4, alpha3);
+	alpha4<= to_sfixed(0.4, alpha4);
 	spikeSynapseSLV(0)<=spikeSynapse;
 	spikeNeuronSLV(0)<= spikeNeuron;
 	v3n<=resize(resize(v3*alpha3,fp_int,fp_frac)+to_sfixed(to_integer(unsigned(spikeSynapseSLV)),fp_int,fp_frac),fp_int,fp_frac);
@@ -91,5 +91,81 @@ begin
 	Wn <=resize(Wsig+deltaW,fp_int,fp_frac);
 
 	WReg : register_fp  port map(clk=>clk,dataIn=>Wn,dataOut=>Wsig,rst=>'0');
+
+end behav;
+
+
+--------------------------- new stdp ----------------------
+
+-- STDP component
+library ieee;
+library ieee_proposed;
+--use model:
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee_proposed.math_utility_pkg.all;
+use ieee_proposed.fixed_pkg.all;
+use work.myTypes.all;
+use work.all;
+
+entity STDP is
+port(spikeSynapse, spikeNeuron, clk, initW : in std_logic;
+		w0: in fp;
+		w: out fp);
+end entity STDP;
+
+architecture behav of STDP is
+
+	component register_fp_en is 
+		port(clk: in std_logic;
+			en : in std_logic;
+			dataIn : in fp;
+			dataOut : out fp);
+	end component;
+
+	component register_fp_rst_1 is 
+		port(clk: in std_logic;
+			rst: in std_logic;
+			dataIn : in fp;
+			dataOut : out fp);
+	end component;
+
+	component decayBlock is 
+		port (spike,clk : in std_logic;
+				alpha : in fp;
+				v: out fp);
+	end component;
+	
+	signal v3,v3n,av3,v4,v4n,av4,avn3,avn4,deltaW,Wn,Wsig : fp:=to_sfixed(0,fp_int,fp_frac);
+	signal alpha1 : fp :=to_sfixed(1,fp_int,fp_frac);
+	signal alpha2 : fp :=to_sfixed(-1,fp_int,fp_frac);
+	signal alpha3 : fp :=to_sfixed(0.4,fp_int,fp_frac);
+	signal alpha4 : fp :=to_sfixed(0.4,fp_int,fp_frac);
+	signal ensig : std_logic;
+
+begin 
+
+	decay_blk_1: decayBlock port map(spike => spikeSynapse, alpha => alpha3, clk => clk, v => v3);
+	decay_blk_2: decayBlock port map(spike => spikeNeuron, alpha => alpha4, clk => clk, v => v4);
+	
+	alpha1<= to_sfixed(1,alpha1);
+	alpha2<= to_sfixed(-1,alpha2);
+	alpha3<= to_sfixed(0.4, alpha3);
+	alpha4<= to_sfixed(0.4, alpha4);
+	
+   deltaW <= resize(alpha1*v3,fp_int,fp_frac) when spikeNeuron = '1' else resize(alpha2*v4,fp_int,fp_frac);
+		
+--	v3n<=resize(resize(v3*alpha3,fp_int,fp_frac)+to_sfixed(to_integer(unsigned(spikeSynapseSLV)),fp_int,fp_frac),fp_int,fp_frac);
+--	v4n<=resize(resize(v4*alpha4,fp_int,fp_frac)+to_sfixed(to_integer(unsigned(spikeNeuronSLV)),fp_int,fp_frac),fp_int,fp_frac);
+	
+--	v3Reg : register_fp_rst_1  port map(clk=>clk,dataIn=>v3n,dataOut=>v3,rst=>spikeSynapse);
+--	v4Reg : register_fp_rst_1  port map(clk=>clk,dataIn=>v4n,dataOut=>v4,rst=>spikeNeuron);
+	
+	W<=Wsig;
+--	deltaW<=resize(alpha1*v3+alpha2*v4,fp_int,fp_frac);
+	ensig <= (spikeNeuron xor spikeSynapse)or initW;
+	
+	Wn <=resize(Wsig+deltaW,fp_int,fp_frac) when initW = '0' else w0;
+	WReg : register_fp_en  port map(clk=>clk, en => ensig, dataIn=>Wn,dataOut=>Wsig);
 
 end behav;
