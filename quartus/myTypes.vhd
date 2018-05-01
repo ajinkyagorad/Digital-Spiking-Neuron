a427990bb8e5cda60870a278935f46fbf8a64a8c
Amritesh Sharma <Amritesh07071996@gmail.com>
2018-05-02 00:05:09 +0530

    Update myTypes.vhd

--- quartus/myTypes.vhd
+++ quartus/myTypes.vhd
@@ -5,8 +5,12 @@ use ieee_proposed.fixed_pkg.all;
 use work.all;
 
 package myTypes is
-constant fp_int : integer :=4;
-constant fp_frac : integer := -3;
-constant fp_bits : integer := fp_int-fp_frac+1;
-subtype fp is sfixed (fp_int downto fp_frac);
+	constant fp_int : integer :=4;
+	constant fp_frac : integer := -10;
+	constant fp_bits : integer := fp_int-fp_frac+1;
+	subtype fp is sfixed (fp_int downto fp_frac);
+
+	type fp_array is array(natural range <>) of fp;
+	type realArray is array(natural range <>) of real;
+	type integerArray is array(natural range <>) of integer;
 end package myTypes;
