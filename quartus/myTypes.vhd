cd46ebfca87abc30a7ff66cc99bf0bbc4eac7067
Amritesh Sharma <Amritesh07071996@gmail.com>
2018-05-04 05:47:54 +0530

    Update myTypes.vhd

--- quartus/myTypes.vhd
+++ quartus/myTypes.vhd
@@ -1,4 +1,3 @@
---this is the file that contains the types and subtypes that are used in the project
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library ieee_proposed;
@@ -6,12 +5,14 @@ use ieee_proposed.fixed_pkg.all;
 use work.all;
 
 package myTypes is
-	constant fp_int : integer :=4; --fp_int is the number of integer bits in the fixed point representation
-	constant fp_frac : integer := -10; --fp_frac is the number of fractional bits in the fixed point representation
-	constant fp_bits : integer := fp_int-fp_frac+1; --fp_bits is the total number of bits in the fixed point representation
-	subtype fp is sfixed (fp_int downto fp_frac); --fp is the fixed point number implemented using the ieee_proposed library sfixed
+	constant fp_int : integer :=4;
+	constant fp_frac : integer := -10;
+	constant fp_bits : integer := fp_int-fp_frac+1;
+	subtype fp is sfixed (fp_int downto fp_frac);
 
 	type fp_array is array(natural range <>) of fp;
 	type realArray is array(natural range <>) of real;
 	type integerArray is array(natural range <>) of integer;
+
+
 end package myTypes;
